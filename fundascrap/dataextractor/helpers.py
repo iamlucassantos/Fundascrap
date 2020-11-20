b391b04ebdc6201aceb41791fb17b2e122dbdb8c
Lucas dos Santos <lucas6eng@gmail.com>
2020-11-20 17:09:10 +0100

    Add functions to scrape

---
+++ fundascrap/dataextractor/helpers.py
@@ -0,0 +1,25 @@
+"""
+Created on 20/11/2020
+
+@author: Lucas V. dos Santos
+"""
+
+import pandas as pd
+import numpy as np
+
+
+
+def json_reader(filename, to_num):
+    df = pd.read_json()
+
+
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
